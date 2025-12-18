5407e4fe14db22b5fe7a295531d87ea32e316f8c
galive007 <alamingalive007@gmail.com>
2025-12-19 03:40:52 +0600

    working roles

--- index.js
+++ index.js
@@ -16,26 +16,26 @@ const decoded = Buffer.from(process.env.FB_KEY, 'base64').toString('utf8')
 const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 
-const verifyFBToken=async(req,res,next)=>{
-    const token=req.headers.authorization
+const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization
 
-    if(!token){
-        return res.status(401).send({message:'unauthorized access'})
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
 
-    try{
-        const idToken=token.split(' ')[1]
-        const decoded=await admin.auth().verifyIdToken(idToken)
-        console.log('decoded info',decoded);
-        req.decoded_email=decoded.email
+    try {
+        const idToken = token.split(' ')[1]
+        const decoded = await admin.auth().verifyIdToken(idToken)
+        console.log('decoded info', decoded);
+        req.decoded_email = decoded.email
         next()
     }
-    catch(error){
-        return res.status(401).send({message:'unauthorized access'})
+    catch (error) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
 }
 
@@ -66,45 +66,71 @@ async function run() {
         app.post('/users', async (req, res) => {
             const userInfo = req.body
             userInfo.createdAt = new Date()
-            userInfo.role='donor'
-            userInfo.status='active'
-            const result=await userCollection.insertOne(userInfo)
+            userInfo.role = 'donor'
+            userInfo.status = 'active'
+            const result = await userCollection.insertOne(userInfo)
             res.send(result)
         })
 
-        app.get('/users',async(req,res)=>{
-            const result=await userCollection.find().toArray()
-            res.send(result)
+        app.get('/users', verifyFBToken, async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.status(200).send(result)
         })
 
         app.get('/users/role/:email',async(req,res)=>{
             const {email}=req.params
-            console.log(email);
+            // console.log(email);
             const query={email:email}
             const result=await userCollection.findOne(query)
-            console.log(result);
-            
+            // console.log(result);
             res.send(result)
         })
+
+        // app.get('/users/:email',async(req,res)=>{
+        //     const email=req.params.email
+        //     const query={email:email}
+        //     const result=await userCollection.findOne(query)
+        //     res.send(result)
+        // })
+
+        app.patch('/update/user/status',verifyFBToken,async(req,res)=>{
+            const {email,status}=req.query
+            const query={email:email}
+
+            const updateStatus={
+                $set:{
+                    status:status
+                }
+            }
+            const result=await userCollection.updateOne(query,updateStatus)
+            res.send(result)
+        })
+
 
 
         // BLOOD Request
-        app.post('/requests',verifyFBToken,async(req,res)=>{
-            const data=req.body
-            data.createdAt=new Date()
-            const result=await bloodRequestsCollection.insertOne(data)
+        app.post('/requests', verifyFBToken, async (req, res) => {
+            const data = req.body
+            data.createdAt = new Date()
+            const result = await bloodRequestsCollection.insertOne(data)
             res.send(result)
         })
 
-        app.get('/donor/requests/:email',async(req,res)=>{
-            const email=req.params.email
-            const query={email:email}
-            const result=await bloodRequestsCollection.find(query).toArray()
+        app.get('/donor/requests/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await bloodRequestsCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.get('/requests',async(req,res)=>{
-            const result=await bloodRequestsCollection.find().toArray()
+        // All-requests
+        // app.get('/requests', async (req, res) => {
+        //     const result = await bloodRequestsCollection.find().toArray()
+        //     res.send(result)
+        // })
+        app.get('/my-request',async(req,res)=>{
+            const email=req.query.email
+            const result=await bloodRequestsCollection.find({email:email}).toArray()
             res.send(result)
         })
 
